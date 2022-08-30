32f5f9184bc692dd67b7da9cd1e91ade29b20887
deadlydev17 <yusuftasliman@gmail.com>
2022-08-31 05:22:53 +0700

    Add member function getTypeMonster and constructor

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <string_view>
 
 class Monster {
 public:
@@ -23,12 +24,41 @@ private:
   int m_hitPoint{};
 
 public:
-  Monster(Type type, const std::string& name, const std::string& roar, int hitPoint) : m_type{type}, m_name{name}, m_roar{roar}, m_hitPoint{hitPoint}{}
+  Monster(Type type, const std::string &name, const std::string &roar,
+          int hitPoint)
+      : m_type{type}, m_name{name}, m_roar{roar}, m_hitPoint{hitPoint} {}
+
+  std::string_view getTypeMonster() const {
+    switch (m_type) {
+    case Type::dragon:
+      return "dragon";
+    case Type::goblin:
+      return "goblin";
+    case Type::ogre:
+      return "ogre";
+    case Type::orc:
+      return "orc";
+    case Type::skeleton:
+      return "skeleton";
+    case Type::troll:
+      return "troll";
+    case Type::vampire:
+      return "vampire";
+    case Type::zombie:
+      return "zombie";
+    default:
+      return "???";
+    }
+  };
+  void print() const {
+    std::cout << m_name << " the " << getTypeMonster() << " has " << m_hitPoint << " hit points and says *" << m_roar << "* \n";
+  }
 };
 
-int main(){
+int main() {
 
-  Monster skeleton{Monster::Type::skeleton, "Bones" , "rattle", 4};
+  Monster skeleton{Monster::Type::skeleton, "Bones", "rattle", 4};
+  skeleton.print();
 
   return 0;
 }
